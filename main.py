b5bda56fdffbd467cc3f02f6d19102a6cad78af4
Probylla <imopri@t-online.de>
2025-08-07 12:29:39 +0200

    word counter added

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def get_book_text (filepath):
         return book_text
 #opens file and reads it as string
 def main():
+    word_list = []
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-    
+    word_list = book_text.split()
+    text_len = len(word_list)
+    print(f"{text_len} words found in the document")
 
 main()
 
